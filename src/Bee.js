add9600a877190e0ee5c8e7f8bb65ce01e6b1683
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2018-10-28 11:35:49 +0200

    6ees6ees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,32 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+	
+	constructor(food,eat) {
+		super(food,eat);
+		this.age = 5;
+		this.color = "yellow";
+		this.job = "Keep on growing";
+	}
+	
 };
+
+/* 
+we can use this way if we want to modify the inherited key or method .... the previous one if only we need to pass them as they are
+
+
+class Bee extends Grub {
+	
+	constructor() {
+		super();
+		this.age = 5;
+		this.color = "yellow";
+		this.job = "Keep on growing";
+	}
+	food() {
+		super.food()
+	}
+	eat() {
+		super.eat()
+	}
+
+};
+*/
